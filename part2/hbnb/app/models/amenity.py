956f596a4e8eb466674654f1928bae0eea549c5b
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-03-02 15:13:19 +0100

    Add Amenity model with validation and update functionality

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,5 +1,5 @@
 from .base_model import BaseModel
-
+from datetime import datetime, timezone
 
 class Amenity(BaseModel):
     """Represent an amenity of a place"""
@@ -11,3 +11,17 @@ class Amenity(BaseModel):
             raise ValueError("Name must be less than 50 characters")
         
         self.name = name
+        self.updated_at = datetime.now(timezone.utc)
+
+    def update(self, name=None):
+        """
+        Update the amenity attributes and refresh the updated_at timestamp.
+
+        Args:
+            name (str, optional): The new name for the amenity.
+        """
+        if name:
+            if len(name) > 50:
+                raise ValueError("Name must be less than 50 characters")
+            self.name = name
+        self.updated_at = datetime.now(timezone.utc)
