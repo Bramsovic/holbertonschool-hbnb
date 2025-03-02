d38e42dbf254cd24190e88b0105e34fb44712be7
Tefa95100 <damien.julien.95100@gmail.com>
2025-03-02 22:26:59 +0100

    Add namespace for api

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_restx import Api
 from app.api.v1.places import api as place_ns
 from app.api.v1.users import api as user_ns
-from app.api.v1.amenities import api as amenity_ns
-from app.api.v1.reviews import api as review_ns
 
 
 def create_app():
@@ -14,8 +12,6 @@ def create_app():
     # Placeholder for API namespaces (endpoints will be added later)
     api.add_namespace(place_ns, path="/api/v1/places")
     api.add_namespace(user_ns, path="/api/v1/users")
-    api.add_namespace(amenity_ns, path="/api/v1/amenities")
-    api.add_namespace(review_ns, path="/api/v1/reviews")
     # Additional namespaces for places, reviews, and amenities will
     # be added later
 
