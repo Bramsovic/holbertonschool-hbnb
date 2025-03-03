e4c0c596485d360341a4928cb7e457490de2a457
Tefa95100 <damien.julien.95100@gmail.com>
2025-03-03 16:58:01 +0100

    Inversion between user_id and place_id

--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -4,7 +4,7 @@ from .base_model import BaseModel
 class Review(BaseModel):
     """Represent an review by the user on the place"""
 
-    def __init__(self, text, rating, place, user):
+    def __init__(self, text, rating, user_id, place_id):
         super().__init__()
 
         if rating < 1 or rating > 5:
@@ -12,5 +12,5 @@ class Review(BaseModel):
 
         self.text = text
         self.rating = rating
-        self.place = place
-        self.user = user
+        self.user_id = user_id
+        self.place_id = place_id
