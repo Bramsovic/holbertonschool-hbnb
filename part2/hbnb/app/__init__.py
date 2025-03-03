1444ff7799de01af973e897e54deb94dd91f7b04
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-03-03 12:09:54 +0100

    Résolution du conflit dans __init__.py après stash pop

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restx import Api
+<<<<<<< Updated upstream
 from app.api.v1.places import api as place_ns
 from app.api.v1.users import api as user_ns
 
@@ -14,5 +15,18 @@ def create_app():
     api.add_namespace(user_ns, path="/api/v1/users")
     # Additional namespaces for places, reviews, and amenities will
     # be added later
+=======
+from app.api.v1.users import api as users_namespace  # Import de ton fichier contenant les routes
+
+def create_app():
+    app = Flask(__name__)
+
+    # Initialisation de l'API Flask-RESTx
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
+
+    # Enregistrement du namespace pour les utilisateurs
+    api.add_namespace(users_namespace, path='/users')
+>>>>>>> Stashed changes
 
     return app
+
