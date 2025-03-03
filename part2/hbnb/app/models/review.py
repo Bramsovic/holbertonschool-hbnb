d8cf2e3bae590caf7e8d8a84b87204c1226619f0
Tefa95100 <damien.julien.95100@gmail.com>
2025-03-03 05:58:52 +0100

    Remove space for pycodestyle

--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -3,13 +3,13 @@ from .base_model import BaseModel
 
 class Review(BaseModel):
     """Represent an review by the user on the place"""
-    
+
     def __init__(self, text, rating, place, user):
         super().__init__()
 
         if rating < 1 or rating > 5:
             raise ValueError("Rating must be between 1 and 5")
-        
+
         self.text = text
         self.rating = rating
         self.place = place
