32b35d7786afb2a36dba0f6a74244b9940fd6524
Tefa95100 <damien.julien.95100@gmail.com>
2025-03-04 14:01:52 +0100

    Remove space for pycodestyle

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -86,7 +86,7 @@ class HBnBFacade:
                 setattr(user, key, value)
 
         user.updated_at = datetime.utcnow()
-        self.user_repo.update(user_id, update_data) 
+        self.user_repo.update(user_id, update_data)
         return user
 
     def get_all_users(self):
