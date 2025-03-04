23f1ae9d4e4772d39ddc2769acb5f528f690c80a
Tefa95100 <damien.julien.95100@gmail.com>
2025-03-04 09:47:08 +0100

    Modification for pycodestyle

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,15 +1,16 @@
 from .base_model import BaseModel
 from datetime import datetime, timezone
 
+
 class Amenity(BaseModel):
     """Represent an amenity of a place"""
-    
+
     def __init__(self, name):
         super().__init__()
 
         if len(name) > 50:
             raise ValueError("Name must be less than 50 characters")
-        
+
         self.name = name
         self.updated_at = datetime.now(timezone.utc)
 
